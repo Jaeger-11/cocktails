582b11a644ad36b2d95005f4ed90337f278998f2
Falodun Damilola <dpaulzs11@gmail.com>
2022-04-19 16:17:00 +0100

    Adding to Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import pages
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+// import components
+import Navbar from './components/Navbar'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} caseSensitive= {false}/>
+        <Route path='/about' element={<About/>} caseSensitive= {false} />
+        <Route path='/cocktail/:id' element={<SingleCocktail/>} caseSensitive= {false} />
+        <Route path='*' element={<Error/>} caseSensitive= {false}/>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
